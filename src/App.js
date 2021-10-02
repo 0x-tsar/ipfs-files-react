721cdf5a28ec1e7c0a2139875fb2117577c9d6be
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-10-02 01:42:20 -0300

    ipfs files js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+/* src/App.js */
+import "./App.css";
+import { useState } from "react";
+import { create } from "ipfs-http-client";
+const client = create("https://ipfs.infura.io:5001/api/v0");
 function App() {
+  const [fileUrl, updateFileUrl] = useState(``);
+  async function onChange(e) {
+    const file = e.target.files[0];
+    console.log(e);
+    console.log(file);
+    try {
+      const added = await client.add(file);
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      updateFileUrl(url);
+      console.log(`url ipfs: ${url}`);
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IPFS Example</h1>
+      <input type="file" onChange={onChange} />
+      {fileUrl && <img src={fileUrl} width="600px" />}
     </div>
   );
 }
-
 export default App;
+
+// import logo from "./logo.svg";
+// import "./App.css";
+// /* import the ipfs-http-client library */
+// import { create } from "ipfs-http-client";
+// import { useEffect, useState } from "react";
+
+// function App() {
+//   useEffect(() => {
+//     /* Create an instance of the client */
+//     const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+//     /* upload the file */
+//     // const added = await client.add(file);
+//     /* or a string */
+//     const added = await client.add("hello world from code!");
+//   }, []);
+//   return <div className="App"></div>;
+// }
+
+// export default App;
